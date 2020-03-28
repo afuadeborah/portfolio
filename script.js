aeaa20ba735d8ed33f21eb86f34f7ad8a2cf46e8
Afua <afrimpong.15@gmail.com>
2020-03-28 18:49:49 -0400

    Changed to li where need be

--- script.js
+++ script.js
@@ -219,14 +219,192 @@ app.init = () => {
         
         m.init();
     }
+
+    const MessengerSkills = function(el){
+        'use strict';
+        let m = this;
+        
+        m.init = function(){
+        m.codeletters = "&#*+%?£@§$";
+        m.message = 0;
+        m.current_length = 0;
+        m.fadeBuffer = false;
+        m.messages = [
+            'Relevant Skills'
+        ];
+        
+        setTimeout(m.animateIn, 100);
+        };
+        
+        m.generateRandomString = function(length){
+        let random_text = '';
+        while(random_text.length < length){
+            random_text += m.codeletters.charAt(Math.floor(Math.random()*m.codeletters.length));
+        } 
+        
+        return random_text;
+        };
+        
+        m.animateIn = function(){
+        if(m.current_length < m.messages[m.message].length){
+            m.current_length = m.current_length + 2;
+            if(m.current_length > m.messages[m.message].length) {
+            m.current_length = m.messages[m.message].length;
+            }
+            
+            let message = m.generateRandomString(m.current_length);
+            $(el).html(message);
+            
+            setTimeout(m.animateIn, 20);
+        } else { 
+            setTimeout(m.animateFadeBuffer, 20);
+        }
+        };
+        
+        m.animateFadeBuffer = function(){
+        if(m.fadeBuffer === false){
+            m.fadeBuffer = [];
+            for(let i = 0; i < m.messages[m.message].length; i++){
+            m.fadeBuffer.push({c: (Math.floor(Math.random()*12))+1, l: m.messages[m.message].charAt(i)});
+            }
+        }
+        
+        let do_cycles = false;
+        let message = ''; 
+        
+        for(let i = 0; i < m.fadeBuffer.length; i++){
+            let fader = m.fadeBuffer[i];
+            if(fader.c > 0){
+            do_cycles = true;
+            fader.c--;
+            message += m.codeletters.charAt(Math.floor(Math.random()*m.codeletters.length));
+            } else {
+            message += fader.l;
+            }
+        }
+        
+        $(el).html(message);
+        
+        if(do_cycles === true){
+            setTimeout(m.animateFadeBuffer, 50);
+        } else {
+            setTimeout(m.cycleText, 2000);
+        }
+        };
+        
+        m.cycleText = function(){
+        m.message = m.message + 1;
+        if(m.message >= m.messages.length){
+            m.message = 0;
+        }
+        
+        m.current_length = 0;
+        m.fadeBuffer = false;
+        $(el).html('Show n Tell');
+        
+        setTimeout(m.animateIn, 500);
+        };
+        
+        m.init();
+    }
+
+    const MessengerProjects = function(el){
+        'use strict';
+        let m = this;
+        
+        m.init = function(){
+        m.codeletters = "&#*+%?£@§$";
+        m.message = 0;
+        m.current_length = 0;
+        m.fadeBuffer = false;
+        m.messages = [
+            'Projects'
+        ];
+        
+        setTimeout(m.animateIn, 100);
+        };
+        
+        m.generateRandomString = function(length){
+        let random_text = '';
+        while(random_text.length < length){
+            random_text += m.codeletters.charAt(Math.floor(Math.random()*m.codeletters.length));
+        } 
+        
+        return random_text;
+        };
+        
+        m.animateIn = function(){
+        if(m.current_length < m.messages[m.message].length){
+            m.current_length = m.current_length + 2;
+            if(m.current_length > m.messages[m.message].length) {
+            m.current_length = m.messages[m.message].length;
+            }
+            
+            let message = m.generateRandomString(m.current_length);
+            $(el).html(message);
+            
+            setTimeout(m.animateIn, 20);
+        } else { 
+            setTimeout(m.animateFadeBuffer, 20);
+        }
+        };
+        
+        m.animateFadeBuffer = function(){
+        if(m.fadeBuffer === false){
+            m.fadeBuffer = [];
+            for(let i = 0; i < m.messages[m.message].length; i++){
+            m.fadeBuffer.push({c: (Math.floor(Math.random()*12))+1, l: m.messages[m.message].charAt(i)});
+            }
+        }
+        
+        let do_cycles = false;
+        let message = ''; 
+        
+        for(let i = 0; i < m.fadeBuffer.length; i++){
+            let fader = m.fadeBuffer[i];
+            if(fader.c > 0){
+            do_cycles = true;
+            fader.c--;
+            message += m.codeletters.charAt(Math.floor(Math.random()*m.codeletters.length));
+            } else {
+            message += fader.l;
+            }
+        }
+        
+        $(el).html(message);
+        
+        if(do_cycles === true){
+            setTimeout(m.animateFadeBuffer, 50);
+        } else {
+            setTimeout(m.cycleText, 2000);
+        }
+        };
+        
+        m.cycleText = function(){
+        m.message = m.message + 1;
+        if(m.message >= m.messages.length){
+            m.message = 0;
+        }
+        
+        m.current_length = 0;
+        m.fadeBuffer = false;
+        $(el).html('Her Work');
+        
+        setTimeout(m.animateIn, 500);
+        };
+        
+        m.init();
+    }
   
     console.clear();
     let messenger = new Messenger($('#messenger'));
 
     let messengerAbout = new MessengerAbout($('#scramble-name'));
 
-// Credits to Codepen: https://codepen.io/bionik/pen/dzBweB?editors=0110
-    // app.Scrambler();
+    let messengerSkills = new MessengerSkills($('#scramble-skill'));
+
+    let messengerProjects = new MessengerProjects($('#scramble-project'));
+
 
 }
 
